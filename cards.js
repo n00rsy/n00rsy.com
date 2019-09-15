70843d2fd64b4e82e74bbb122e50a1a4a6f9387b
Noor Syed <noorsyed1a@gmail.com>
2019-09-15 00:05:03 -0400

    working reusable card with reacjs

--- cards.js
+++ cards.js
@@ -1,5 +1,3 @@
-'use strict';
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -8,37 +6,131 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var e = React.createElement;
+var CardHeader = function (_React$Component) {
+  _inherits(CardHeader, _React$Component);
 
-var LikeButton = function (_React$Component) {
-  _inherits(LikeButton, _React$Component);
+  function CardHeader() {
+    _classCallCheck(this, CardHeader);
 
-  function LikeButton(props) {
-    _classCallCheck(this, LikeButton);
-
-    var _this = _possibleConstructorReturn(this, (LikeButton.__proto__ || Object.getPrototypeOf(LikeButton)).call(this, props));
-
-    _this.state = { liked: false };
-    return _this;
+    return _possibleConstructorReturn(this, (CardHeader.__proto__ || Object.getPrototypeOf(CardHeader)).apply(this, arguments));
   }
 
-  _createClass(LikeButton, [{
+  _createClass(CardHeader, [{
     key: 'render',
     value: function render() {
-      var _this2 = this;
+      var image = this.props.image;
 
-      if (this.state.liked) {
-        return 'You liked this.';
-      }
-
-      return e('button', { onClick: function onClick() {
-          return _this2.setState({ liked: true });
-        } }, 'Like');
+      var style = {
+        backgroundImage: 'url(' + image + ')'
+      };
+      return React.createElement(
+        'header',
+        { style: style, id: image, className: 'card-header' },
+        React.createElement(
+          'h4',
+          { className: 'card-header--title' },
+          'Rutgers Course Sniper'
+        )
+      );
     }
   }]);
 
-  return LikeButton;
+  return CardHeader;
 }(React.Component);
 
-var domContainer = document.querySelector('#content-wrapper');
-ReactDOM.render(e(LikeButton), domContainer);
+function goToURL(url) {
+  window.location.href = url;
+}
+
+var Button = function (_React$Component2) {
+  _inherits(Button, _React$Component2);
+
+  function Button() {
+    _classCallCheck(this, Button);
+
+    return _possibleConstructorReturn(this, (Button.__proto__ || Object.getPrototypeOf(Button)).apply(this, arguments));
+  }
+
+  _createClass(Button, [{
+    key: 'render',
+    value: function render() {
+      var _this3 = this;
+
+      return React.createElement(
+        'button',
+        { className: 'button button-primary', onClick: function onClick() {
+            return goToURL(_this3.props.url);
+          } },
+        React.createElement('i', { className: 'fa fa-chevron-right' }),
+        ' Find out more'
+      );
+    }
+  }]);
+
+  return Button;
+}(React.Component);
+
+var CardBody = function (_React$Component3) {
+  _inherits(CardBody, _React$Component3);
+
+  function CardBody() {
+    _classCallCheck(this, CardBody);
+
+    return _possibleConstructorReturn(this, (CardBody.__proto__ || Object.getPrototypeOf(CardBody)).apply(this, arguments));
+  }
+
+  _createClass(CardBody, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'div',
+        { className: 'card-body' },
+        React.createElement(
+          'h2',
+          null,
+          this.props.title
+        ),
+        React.createElement(
+          'p',
+          { className: 'body-content' },
+          this.props.text
+        ),
+        React.createElement(Button, { url: this.props.url })
+      );
+    }
+  }]);
+
+  return CardBody;
+}(React.Component);
+
+var Card = function (_React$Component4) {
+  _inherits(Card, _React$Component4);
+
+  function Card() {
+    _classCallCheck(this, Card);
+
+    return _possibleConstructorReturn(this, (Card.__proto__ || Object.getPrototypeOf(Card)).apply(this, arguments));
+  }
+
+  _createClass(Card, [{
+    key: 'render',
+    value: function render() {
+      return React.createElement(
+        'article',
+        { className: 'card' },
+        React.createElement(CardHeader, { image: this.props.image, title: this.props.name }),
+        React.createElement(CardBody, { title: this.props.header, text: this.props.description, url: this.props.codeURL })
+      );
+    }
+  }]);
+
+  return Card;
+}(React.Component);
+
+ReactDOM.render(React.createElement(Card, {
+  name: 'big brain timerrr',
+  description: 'ayaks crowd Three Sisteayaks crowd Three Sisteayaks crowd Three Sisteayaks crowd Three Siste',
+  image: 'https://source.unsplash.com/user/erondu/600x400',
+  header: 'JavaScript+ Node.js+ AWS EC2',
+  codeURL: 'https://github.com/n00rsy/Rutgers-Auto-Course-Register'
+}), document.getElementById('app'));
--- src/cards.js
+++ src/cards.js
@@ -1,24 +1,67 @@
-'use strict';
 
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
+class CardHeader extends React.Component {
   render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
+    const { image } = this.props;
+    var style = { 
+        backgroundImage: 'url(' + image + ')',
+    };
+    return (
+      <header style={style} id={image} className="card-header">
+        <h4 className="card-header--title">Rutgers Course Sniper</h4>
+      </header>
+    )
   }
 }
-const domContainer = document.querySelector('#content-wrapper');
-ReactDOM.render(e(LikeButton), domContainer);
+function goToURL(url){
+	window.location.href=url;
+}
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button className="button button-primary" onClick={() => goToURL(this.props.url)}>
+        <i className="fa fa-chevron-right"></i> Find out more
+      </button>
+    )
+  }
+}
+
+class CardBody extends React.Component {
+  render() {
+    return (
+      <div className="card-body">
+        
+        
+        <h2>{this.props.title}</h2>
+        
+        <p className="body-content">{this.props.text}</p>
+        
+        <Button url = {this.props.url} />
+      </div>
+    )
+  }
+}
+
+class Card extends React.Component {
+  render() {
+    return (
+      <article className="card">
+        <CardHeader image={this.props.image} title = {this.props.name}/>
+		
+	
+  <CardBody title={this.props.header} text={this.props.description} url = {this.props.codeURL}/>
+      </article>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Card 
+  name ="big brain timerrr"
+  description = "ayaks crowd Three Sisteayaks crowd Three Sisteayaks crowd Three Sisteayaks crowd Three Siste"
+  image = 'https://source.unsplash.com/user/erondu/600x400'
+  header = "JavaScript+ Node.js+ AWS EC2"
+  codeURL='https://github.com/n00rsy/Rutgers-Auto-Course-Register'
+  />,
+  document.getElementById('app')
+);
